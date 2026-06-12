0c9eeee7331eb8d5d68b9a8f667df0cb52b54f81
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-12 20:13:21 +0200

    App.jsx functions added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,57 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>
+  const [thoughts, setThoughts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const API_URL = "https://happy-thoughts-api-4ful.onrender.com/thoughts"
+
+  // fetch 20 latest thoughts from api
+  const fetchThoughts = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(API_URL)
+      const data = await response.json()
+      setThoughts(data)
+    } catch (error) {
+      console.error("Could not fetch thoughts:", error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchThoughts()
+  }, [])
+
+  // function to add new thoughts 
+  const addNewThought = (newThought) => {
+    setThoughts((prevThoughts) => [newThought, ...prevThoughts])
+  }
+
+  // function to add hearts
+  const handleLikeThought = (thoughtId) => {
+    setThoughts((prevThoughts) =>
+      prevThoughts.map((thought) =>
+        thought._id === geopoliticalId
+          ? { ...thought, hearts: thought.hearts + 1 }
+          : thought
+      )
+    )
+  }
+
+  return (
+    <div className="main-container">
+      <header>
+        <h1>🌿 Happy Thoughts</h1>
+        <p>Spread some positivity into the ether.</p>
+      </header>
+
+      {/* add thoughtform and thoughtlist soon */}
+      <div className="placeholder-info">
+        {loading ? <p>Loading happy thoughts...</p> : <p>Loading complete! Totally {thoughts.length} thoughts loaded.</p>}
+      </div>
+    </div>
+  )
 }
