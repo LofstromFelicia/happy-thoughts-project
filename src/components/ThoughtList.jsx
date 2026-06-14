e16f055ece17e99fd10e9bee490f550ecb453fbf
Felicia Löfström <Flizan_Linnea@hotmail.com>
2026-06-14 12:19:27 +0200

    add favicon & interactive like button

--- src/components/ThoughtList.jsx
+++ src/components/ThoughtList.jsx
@@ -1,19 +1,22 @@
+import { useState } from "react"
 import { formatDistanceToNow } from "date-fns"
 import { enUS } from "date-fns/locale"
 
 const ThoughtList = ({ thoughts, API_URL, onLike }) => {
+  const [likedThoughts, setLikedThoughts] = useState([])
 
   const handleLikeClick = async (thoughtId) => {
     onLike(thoughtId)
 
+    if (!likedThoughts.includes(thoughtId)) {
+      setLikedThoughts((prevLiked) => [...prevLiked, thoughtId])
+    }
+
     try {
-      const response = await fetch(`${API_URL}/${thoughtId}/like`, {
+      await fetch(`${API_URL}/${thoughtId}/like`, {
         method: "POST",
         headers: { "Content-Type": "application/json" }
       })
-
-      if (!response.ok) {
-      }
     } catch (error) {
       console.error("Could not like thought:", error)
     }
@@ -21,28 +24,33 @@ const ThoughtList = ({ thoughts, API_URL, onLike }) => {
 
   return (
     <section className="thought-list">
-      {thoughts.map((thought) => (
-        <div key={thought._id} className="thought-card">
-          <p className="thought-message">{thought.message}</p>
+      {thoughts.map((thought) => {
+        const isLikedByMe = likedThoughts.includes(thought._id)
 
-          <div className="thought-footer">
-            <div className="like-section">
-              <button
-                onClick={() => handleLikeClick(thought._id)}
-                className={`heart-button ${thought.hearts > 0 ? "has hearts" : ""}`}
-              >
-                ❤️
-              </button>
-              <span className="like-count">x {thought.hearts}</span>
+        return (
+          <div key={thought._id} className="thought-card" >
+            <p className="thought-message">{thought.message}</p>
+
+            <div className="thought-footer">
+              <div className="like-section">
+                <button
+                  onClick={() => handleLikeClick(thought._id)}
+                  className={`heart-button ${isLikedByMe ? "has hearts" : ""}`}
+                  style={{ backgroundColor: isLikedByMe ? "#ffb3c1" : "#eaeaea" }}
+                >
+                  ❤️
+                </button>
+                <span className="like-count">x {thought.hearts}</span>
+              </div>
+
+              <p className="thought-time">
+                {formatDistanceToNow(new Date(thought.createdAt), { addSuffix: true, locale: enUS })}
+              </p>
             </div>
-
-            <p className="thought-time">
-              {formatDistanceToNow(new Date(thought.createdAt), { addSuffix: true, locale: enUS })}
-            </p>
           </div>
-        </div>
-      ))}
-    </section>
+        )
+      })}
+    </section >
   )
 }
 
